e20b0107211541f9ee9660b9b3094ab448bc8d72
sjno <sitegun@naver.com>
2017-11-08 11:07:32 -0800

    Change a master

--- PMon/src/Main.java
+++ PMon/src/Main.java
@@ -1,4 +1,11 @@
 
 public class Main {
-
+	public static void main(String args[])
+	{
+		Show("Master");
+	}
+	public static void Show(String owner)
+	{
+		System.out.println("this is " + owner);
+	}
 }
