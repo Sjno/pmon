30a440917272bef2e2250d6a9334b92c6e08968f
sjno <sitegun@naver.com>
2017-11-08 11:35:59 -0800

    feature 2

--- PMon/src/Main.java
+++ PMon/src/Main.java
@@ -3,9 +3,14 @@ public class Main {
 	public static void main(String args[])
 	{
 		Show("Master");
+		ShowBrans();
 	}
 	public static void Show(String owner)
 	{
 		System.out.println("this is " + owner);
+	}
+	public static void ShowBrans()
+	{
+		System.out.println("branch!");
 	}
 }
